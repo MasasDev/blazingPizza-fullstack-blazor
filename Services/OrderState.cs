8d5f40c10c9139e5565450a2c635a5831e9f36f9
MazasDev <123668720+MasasDev@users.noreply.github.com>
2025-03-28 12:52:31 +0000

    feat: add pizza configuration dialog for size selection
    - Implemented a dialog for selecting pizza size.
    - Managed dialog state using `OrderState`.
    - Confirm and cancel buttons close the dialog without passing additional data.

---
+++ Services/OrderState.cs
@@ -0,0 +1,34 @@
+namespace BlazingPizza.Services;
+
+public class OrderState
+{
+    public Order Order{get; private set;} = new Order();
+    public bool ShowConfigureDialog{get; private set;}
+    public Pizza ConfiguringPizza{get; private set;}
+
+    public void ShowConfigurePizzaDialog(PizzaSpecial special)
+    {
+        ConfiguringPizza = new Pizza
+        {
+            Special = special,
+            SpecialId = special.Id,
+            Size = Pizza.DefaultSize,
+            Toppings = new List<PizzaTopping>()
+        };
+        ShowConfigureDialog = true;
+    }
+    public void ConfirmConfigurePizzaDialog()
+    {
+        Order.Pizzas.Add (ConfiguringPizza);
+
+        ConfiguringPizza = null;    
+        
+        ShowConfigureDialog = false;
+    }
+    public void CancelConfigurePizzaDialog()
+    {
+        ConfiguringPizza = null;
+
+        ShowConfigureDialog = false;
+    }
+}
